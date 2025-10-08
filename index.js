1bf745319793c9a3390819b89bbfde17eab80e72
texas7xp <159555851+texas7xp@users.noreply.github.com>
2025-10-08 10:35:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // 🔹 Conexão com seu banco PostgreSQL no Render
 const pool = new Pool({
   connectionString:
-    "postgresql://chat_db_93e6_user:xWGz5hB1P2PJFaZvrcgzzvg1tt3HSAsH@dpg-d3j1vvt6ubrc73a06240-a.oregon-postgres.render.com/chat_db_93e6",
+    "postgresql://chat_db_udk5_user:OAG7GgxVgAqk4dfFJ04v4LHTBfaq6knV@dpg-d3j6db1gv73c73dp5kj0-a.oregon-postgres.render.com/chat_db_udk5",
   ssl: { rejectUnauthorized: false },
 });
 
